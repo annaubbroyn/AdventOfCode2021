b62e474d530c050193532c08d88f63eb528b6130
Anna Brøyn <anna.broyn@gmail.com>
2021-12-09 20:03:38 +0100

    Day 8 complete......

--- Day8/day8_part2.py
+++ Day8/day8_part2.py
@@ -48,10 +48,124 @@ def initial_mapping(original,input):
             break
     return mapping
 
+def find0(original,mapping,input):
+    candidates = []
+    for digit in input:
+        if len(digit) == 6:
+            candidates.append(digit)
+    for digit in candidates:
+        if list(mapping['c'])[0] in digit and list(mapping['c'])[1] in digit and list(mapping['e'])[0] in digit and list(mapping['e'])[1] in digit:
+            return ''.join(sorted(digit))
+    return None
 
+def find1(original,mapping,input):
+    for digit in input:
+        if len(digit) == 2:
+            return ''.join(sorted(digit))
+    return None        
 
-f = open("input_short.txt","r")
+def find2(original,mapping,input):
+    candidates = []
+    for digit in input:
+        if len(digit) == 5:
+            candidates.append(digit)
+    for digit in candidates:
+        if list(mapping['e'])[0] in digit and list(mapping['e'])[1] in digit:
+            return ''.join(sorted(digit))
+    return None
+
+def find3(original,mapping,input):
+    candidates = []
+    for digit in input:
+        if len(digit) == 5:
+            candidates.append(digit)
+    for digit in candidates:
+        if list(mapping['c'])[0] in digit and list(mapping['c'])[1] in digit:
+            return ''.join(sorted(digit))
+    return None
+
+def find4(original,mapping,input):
+    for digit in input:
+        if len(digit) == 4:
+            return ''.join(sorted(digit))
+    return None
+
+def find5(original,mapping,input):
+    candidates = []
+    for digit in input:
+        if len(digit) == 5:
+            candidates.append(digit)
+    for digit in candidates:
+        if list(mapping['b'])[0] in digit and list(mapping['b'])[1] in digit:
+            return ''.join(sorted(digit))
+    return None
+
+def find6(original,mapping,input):
+    candidates = []
+    for digit in input:
+        if len(digit) == 6:
+            candidates.append(digit)
+    for digit in candidates:
+        if list(mapping['b'])[0] in digit and list(mapping['b'])[1] in digit and list(mapping['e'])[0] in digit and list(mapping['e'])[1] in digit:
+            return ''.join(sorted(digit))
+    return None
+
+def find7(original,mapping,input):
+    for digit in input:
+        if len(digit) == 3:
+            return ''.join(sorted(digit))
+    return None
+
+def find8(original,mapping,input):
+    for digit in input:
+        if len(digit) == 7:
+            return ''.join(sorted(digit))
+    return None
+
+def find9(original,mapping,input):
+    candidates = []
+    for digit in input:
+        if len(digit) == 6:
+            candidates.append(digit)
+    for digit in candidates:
+        if list(mapping['b'])[0] in digit and list(mapping['b'])[1] in digit and list(mapping['c'])[0] in digit and list(mapping['c'])[1] in digit:
+            return ''.join(sorted(digit))
+    return None
+
+f = open("input.txt","r")
 mapping = initial_mapping(original,input)
-print(mapping)
-#print(final_mapping(mapping,original,input))
+#print(mapping)
+#print(find9(original,mapping,input))
+
+sum = 0
+for line in f:
+    input = (line.split('|')[0]).split()
+    output = (line.split('|')[1]).split()
+    mapping = initial_mapping(original,input)
+    digit_mapping = []
+    digit_mapping.append(find0(original,mapping,input))
+    digit_mapping.append(find1(original,mapping,input))
+    digit_mapping.append(find2(original,mapping,input))
+    digit_mapping.append(find3(original,mapping,input))
+    digit_mapping.append(find4(original,mapping,input))
+    digit_mapping.append(find5(original,mapping,input))
+    digit_mapping.append(find6(original,mapping,input))
+    digit_mapping.append(find7(original,mapping,input))
+    digit_mapping.append(find8(original,mapping,input))
+    digit_mapping.append(find9(original,mapping,input))
+    #print(digit_mapping)
+    #for o in output:
+    #    print(o)
+    #    print(digit_mapping.index(''.join(sorted(o))))
+    #    part_sum
+    #print('\n')
+    part_sum = digit_mapping.index(''.join(sorted(output[0])))*1000 + digit_mapping.index(''.join(sorted(output[1])))*100 + digit_mapping.index(''.join(sorted(output[2])))*10 + digit_mapping.index(''.join(sorted(output[3])))
+    #print(part_sum)
+    sum += part_sum
+
+print(sum)
+
+
+
+
 
